09390655f526f75565018c00b7912b648fbabfa7
ezzattarek14 <158750239+ezzattarek14@users.noreply.github.com>
2025-09-18 17:58:43 +0300

    Update app.js

--- foldername_real/app.js
+++ foldername_real/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors')
-require('dotenv').config();  
+//require('dotenv').config();  
 
 
 
